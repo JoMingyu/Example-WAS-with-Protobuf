20173be0e8fc58c8a263d466a918cdd2d535718c
JoMingyu <mingyu.planb@gmail.com>
2019-02-01 02:38:53 +0900

    signup 다 만듬 ㅎ.ㅎ

--- app/views/user/signup.py
+++ app/views/user/signup.py
@@ -1,10 +1,28 @@
-from flask import request
+from flask import abort, g
+from peewee import IntegrityError
 
+from app.decorators.protobuf import receive_protobuf
+from app.models.user import TblUsers
 from app.views.base import BaseResource
+from app.views.user.user_pb2 import SignupRequest
 
 
 class Signup(BaseResource):
+    @receive_protobuf(SignupRequest)
     def post(self):
-        payload = request.json
+        payload = g.request
 
-        return payload, 201
+        id = payload.id
+        pw = payload.pw
+        name = payload.name
+
+        try:
+            TblUsers.insert(
+                id=id,
+                pw=pw,
+                name=name
+            ).execute()
+        except IntegrityError:
+            abort(409)
+
+        return {}, 201
