77969de44829c5fb5361461e2c03483ac0fb2230
JoMingyu <mingyu.planb@gmail.com>
2019-02-01 03:23:41 +0900

    tbl_posts에 author_id 추가

--- app/models/post.py
+++ app/models/post.py
@@ -2,10 +2,12 @@ from peewee import *
 
 from app.extensions import db
 from app.models import BaseModel
+from app.models.user import TblUsers
 
 
 class TblPosts(BaseModel):
     content = CharField(max_length=4095)
+    author_id = ForeignKeyField(TblUsers, db_column='author_id', to_field='id')
 
     class Meta:
         database = db
